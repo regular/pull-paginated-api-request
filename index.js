bbd38306f7575412b42234b4915fbb740e9fc632
Jan Bölsche <regular.gonzales@googlemail.com>
2015-09-14 14:50:18 +0200

    Move creation of REST response stream to our consumer. Add test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // jshint -W061
-var hyperquest = require('hyperquest');
 var concat = require('concat-stream');
+var pathway = require('pathway');
 
 var debug = require('debug')('pull-paginated-api-request');
 var pull = require('pull-stream');
@@ -8,18 +8,47 @@ var generate = require('pull-generate');
 
 // return a function that retuens a stream of objects
 // the next page is requested automatically when needed
-module.exports = function (makeUrl) {
-    return function request(url, query, extract) {
+// arguments:
+// - makeRequest: function(query, pageToken) return api response as node-style stream
+module.exports = function (makeRequest) {
+    // arguments:
+    // - query: passed to makeRequest
+    // - extract:
+    //   - either: function(responseString, cb)
+    //     - that calls cb(err, items, nextPageToken)
+    //   - or: array, interpreted as a pathway
+    //     - first array entry is the items path
+    //     - 2nd array entry is the nextPageToken path
+    return function request(query, extract) {
+        if (extract instanceof Array) {
+            var ways = extract;
+            extract = function(response, cb) {
+                var obj;
+                try {
+                    obj = JSON.parse(response);
+                } catch(err) {
+                    debug('JSON parse error: %s', err.message);
+                    console.log(response.toString());
+                    return cb(err);
+                }
+                var items = pathway(obj, ways[0]);
+                debug('items are %s', ways[0], typeof items);
+                if (typeof items === 'undefined') return cb(new Error('no items extracted'));
+                var nextToken = ways.length>1 ? pathway(obj, ways[1]) : undefined;
+                cb(null, items, nextToken[0]);
+            };
+        }
         return pull(
             generate(null, function(pageToken, cb) {
                 if (typeof pageToken === 'undefined') return cb(true);
-                debug('requesting %s page %s', url, pageToken);
-                hyperquest(makeUrl(url, query, pageToken))
-                .pipe(concat(function(json) {
-                    var result = extract(json);
-                    var items = result[0];
-                    var nextToken = result[1];
-                    cb(null, items, nextToken);
+                debug('requesting %s page %s', query, pageToken);
+                var responseStream = makeRequest(query, pageToken);
+                responseStream.on('error', function(err) {
+                    debug('error: %s', err.message);
+                    cb(err);
+                });
+                responseStream.pipe(concat(function(response) {
+                    extract(response, cb);
                 }));
             }),
             pull.flatten()
---
+++ test.js
@@ -0,0 +1,140 @@
+var par = require('./');
+var test = require('tape');
+var debug = require('debug')('test');
+var pull = require('pull-stream');
+var bl = require('bl');
+var through = require('through');
+
+function mock(t, expectedQuery, expectedToken, responses, opts) {
+    opts = opts ||{};
+    var fail = opts.fail;
+    var i = 0;
+    return function makeRequest(query, token) {
+        debug('make request for %s with token=%s', query, token);
+        if (fail) debug('(will fail)');
+        t.deepEqual(query, expectedQuery[i]);
+        t.equal(token, expectedToken[i]);
+        var responseStream;
+       
+        if (fail) {
+            responseStream = through(function(err) {
+                this.emit('error', err);
+            });
+            setTimeout(function() {
+                responseStream.write(fail);
+            }, 250);
+        } else {
+            responseStream = bl();
+            responseStream.append(responses[i]);
+        }
+        i++;
+        return responseStream;
+    };
+}
+
+test('immediately aborts pull-stream source when token === undefined', function(t) {
+    var expectedValues = [1,2,3];
+    var req = par(mock(t, ['query'],[null],['response']));
+    var stream = req('query', function(response, cb) {
+        debug('extract called');
+        t.equal(response.toString(), 'response');
+        cb(null, expectedValues, undefined);}
+    );
+    pull(
+        stream,
+        pull.collect(function(end, values) {
+            t.equal(end, null);
+            t.deepEqual(values, expectedValues);
+            t.end();
+        })
+    );
+});
+
+test('pass error during request', function(t) {
+    var err = new Error('hey');
+    var req = par(mock(t, ['query'],[null],['response'], {fail: err}));
+    var stream = req('query', function(response, cb) {
+        t.fail('should not call extract');
+    });
+    pull(
+        stream,
+        pull.collect(function(end, values) {
+            t.equal(end, err);
+            t.deepEqual(values, []);
+            t.end();
+        })
+    );
+});
+
+test('pass error during extract', function(t) {
+    var err = new Error('hey');
+    var req = par(mock(t, ['query'],[null],['response']));
+    var stream = req('query', function(response, cb) {
+        t.equal(response.toString(), 'response');
+        cb(err);
+    });
+    pull(
+        stream,
+        pull.collect(function(end, values) {
+            t.equal(end, err);
+            t.deepEqual(values, []);
+            t.end();
+        })
+    );
+});
+
+test('pass on what has been extracted from 2 pages', function(t) {
+    var req = par(mock(t, ['query', 'query'],[null, 'page1'],['[1,2,3]', '[4,5,6]']));
+    var i = 0;
+    var stream = req('query', function(response, cb) {
+        cb(null, JSON.parse(response), i++ === 0 ? 'page1' : undefined);
+    });
+    pull(
+        stream,
+        pull.collect(function(end, values) {
+            t.equal(end, null);
+            t.deepEqual(values, [1,2,3,4,5,6]);
+            t.end();
+        })
+    );
+});
+
+test('support pathways in place of extract functions', function(t) {
+    var req = par(mock(t, ['query', 'query'],[null, 'page1'],['{"items": [1,2,3], "token": "page1"}', '{"items": [4,5,6]}']));
+    var i = 0;
+    var stream = req('query', [['items', true],['token']]);
+    pull(
+        stream,
+        pull.collect(function(end, values) {
+            t.equal(end, null);
+            t.deepEqual(values, [1,2,3,4,5,6]);
+            t.end();
+        })
+    );
+});
+
+test('default extract (pathways) - error in JSON', function(t) {
+    var req = par(mock(t, ['query'],[null],['glibberish']));
+    var stream = req('query', [['items', true],['token']]);
+    pull(
+        stream,
+        pull.collect(function(err, values) {
+            t.ok(err, 'should be an error');
+            t.deepEqual(values, []);
+            t.end();
+        })
+    );
+});
+
+test('default extract (pathways) - non-existing items', function(t) {
+    var req = par(mock(t, ['query'],[null],['{}']));
+    var stream = req('query', [['items', true],['token']]);
+    pull(
+        stream,
+        pull.collect(function(err, values) {
+            t.notOk(err, 'should not be an error');
+            t.deepEqual(values, []);
+            t.end();
+        })
+    );
+});
